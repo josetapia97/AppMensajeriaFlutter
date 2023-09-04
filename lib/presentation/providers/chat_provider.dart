5d5581648f8e4c589aa70ce35afa7c8f6c8b8ec4
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-04 15:36:00 -0300

    API: Conexion y mapeo

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messagesList = [
     Message(text: 'Hola amor!', fromWho: FromWho.me),
@@ -14,13 +16,24 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      hisReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> hisReply() async {
+    final hisMessage = await getYesNoAnswer.getAnswer();
+    messagesList.add(hisMessage);
     notifyListeners();
     moveScrollToBottom();
   }
 
   Future<void> moveScrollToBottom() async {
     await Future.delayed(const Duration(milliseconds: 100));
-    
+
     chatScrollController.animateTo(
         chatScrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 300),
