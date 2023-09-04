711d58e35cc55f3fabca19594d58665ed033620b
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-04 14:49:33 -0300

    Mensajes y scroll auto

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,12 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  final ScrollController chatScrollController = ScrollController();
+
+  List<Message> messagesList = [
     Message(text: 'Hola amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
   ];
 
-  Future<void> sendMessage(String text) async{
-    //ToDo: implementar metodo
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messagesList.add(newMessage);
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/his_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/shared/message_field_box.dart';
@@ -8,6 +11,8 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //como mi widget no tiene estado, ahora quien maneja mi info es provider
+
     return Scaffold(
       appBar: AppBar(
         leading: const Padding(
@@ -28,16 +33,27 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(itemBuilder: ((context, index) {
-              return (index % 2 == 0) ? HisMessageBubble() : MyMessageBubble();
-            }))),
+            Expanded(
+                child: ListView.builder(
+                  controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messagesList.length,
+                    itemBuilder: ((context, index) {
+                      final message = chatProvider.messagesList[index];
+                      return (message.fromWho == FromWho.his)
+                          ? const HisMessageBubble()
+                          : MyMessageBubble(message: message);
+                    }))),
             //caja de texto
-            MessageFieldBox(),
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
           ],
         ),
       ),
